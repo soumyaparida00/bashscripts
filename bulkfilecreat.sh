e53dda42ca664b24b1fb2b4dfc896cbf8e253c03
soumyaparida00 <soumyaparida00@gmail.com>
2023-04-16 16:57:09 +0530

    Update bulkfilecreat.sh

--- bulkfilecreat.sh
+++ bulkfilecreat.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# This script will create 500 files from file.txt to file500.txt. And each file will contain the name of the file in it and,
-then all the files will be tar.gz'd to another file.
+# This script will create 500 files from file.txt to file500.txt. And each file will contain the name of the file in it and, then all the files will be tar.gz'd to another file.
 
 for i in {000..500}
 do 
